86aa9f9c968c04b81efb4f2da68a72acc6c6b636
Udhay <udhayak4251@gmail.com>
2020-04-26 22:55:51 +0530

    latest

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import { by, element, ElementFinder } from "protractor";
-export class CommonLocators {
+const { by, element } =  require('protractor');
+export class CommonElement {
     /**
      * @params 
      * xpath: xpath as input string
      * rootElement: ElementFinder
      */
-    public getElementByXpath(xpath: string, rootElement?: ElementFinder) {
+    public getElementByXpath(xpath: string, rootElement?: any) {
         return rootElement? rootElement.element(by.xpath(xpath)): element(by.xpath(xpath));
+    }
+}
+
+export class CommonElements {
+    /**
+     * @params 
+     * xpath: xpath as input string
+     * rootElement: ElementFinder
+     */
+    public getAllElementsByXpath(xpath: string, rootElement?: any) {
+        return rootElement? rootElement.element.all(by.xpath(xpath)): element.all(by.xpath(xpath));
     }
 }
